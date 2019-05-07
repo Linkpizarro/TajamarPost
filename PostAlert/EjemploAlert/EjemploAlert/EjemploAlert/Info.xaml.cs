34fbc4937807c46d23d2cc6cea4f46e8df8059f1
Unknown <elcentaleon@gmail.com>
2019-05-07 11:35:24 +0200

    proyecto alert

---
+++ PostAlert/EjemploAlert/EjemploAlert/EjemploAlert/Info.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace EjemploAlert
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Info : ContentPage
+	{
+		public Info()
+		{
+			InitializeComponent ();
+            this.close.Clicked += async (sender, args) =>
+            {
+                await Navigation.PopModalAsync();
+            };
+        }
+	}
+}
---
+++ PostAlert/EjemploAlert/EjemploAlert/EjemploAlert/MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace EjemploAlert
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            bool termresult = false;
+            String sexresult = null;
+            this.register.Clicked += async (sender, args) =>
+            {
+                this.result.Text = "";
+                if(this.name.Text != null && this.surname.Text != null && this.age.Text != null && sexresult != null)
+                {
+                    if (termresult)
+                    {
+                        this.result.Text = "Registrado correctamente.";
+                    }
+                    else
+                    {
+                        await DisplayAlert("Advertencia", "No ha aceptado los terminos.", "Cerrar");
+                    }
+                   
+                }
+                else
+                {
+                    await DisplayAlert("Advertencia", "Hay campos si rellenar.", "Cerrar");
+                }
+               
+            };
+            this.sex.Clicked += async (sender, args) =>
+            {
+                sexresult = await DisplayActionSheet("Selección de sexo", "Cancelar", null, "Hombre", "Mujer", "Prefiero no contestar");
+            };
+            this.terms.Clicked += async (sender, args) =>
+            {
+                termresult = await DisplayAlert("Términos de Uso", "¿Acepta nustra política de uso de datos?", "Si", "No");
+            };
+            this.info.Clicked += async (sender, args) =>
+            {
+                Info modal = new Info();
+                await Navigation.PushModalAsync(modal);
+            };
+        }
+    }
+}
